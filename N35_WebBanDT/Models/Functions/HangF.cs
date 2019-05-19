185d517de714a64e368abc946fb928adc50c101a
QuangAnh <quanganh5086@gmail.com>
2019-05-19 18:43:22 +0700

    Add project

---
+++ N35_WebBanDT/Models/Functions/HangF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using N35_WebBanDT.Models.Entities;
+namespace N35_WebBanDT.Models.Functions
+{
+    public class HangF
+    {
+        private MyDBContext context;
+        public HangF()
+        {
+            context = new MyDBContext();
+        }
+        public IQueryable<Hang> Hangs
+        {
+            get { return context.Hangs; }
+        }
+    }
+}
